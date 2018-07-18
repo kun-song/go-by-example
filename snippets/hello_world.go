3f1323558811ec108fbb4c2df4e63486ef2cd119
Song Kun <songkun.fp@gmail.com>
2018-07-18 08:41:44 +0800

    init

---
+++ snippets/hello_world.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+/**
+ * 两种运行方式：
+ *   1. go run hello_world.go
+ *   2. go build hello_world.go，然后 ./hello-world
+ */
+func main() {
+	fmt.Println("Hello world!")
+}
