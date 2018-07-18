864de3c4d37c7de8e7ed03d8f721fcf504d9b57f
Song Kun <songkun.fp@gmail.com>
2018-07-18 12:40:58 +0800

    Update values.go

---
+++ snippets/values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+/**
+ * Go 基本类型：string, integer, float, boolean
+ */
+func main() {
+	fmt.Println("go" + "lang")
+	
+	fmt.Println("1 + 1 =", 1 + 1)
+	fmt.Println("7.0 / 3.0 =", 7.0 / 3.0)
+	
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
