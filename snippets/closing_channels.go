b6d328fba00c496f2dea52739aee592117cccb20
Song Kun <songkun.fp@gmail.com>
2019-04-11 21:18:36 +0800

    Update closing_channels.go

--- snippets/closing_channels.go
+++ snippets/closing_channels.go
@@ -10,6 +10,9 @@ func main() {
 
 	/**
 	 * 若 jobs 被关闭，且 jobs 中所有消息都被处理完，则 more 为 false
+	 *
+	 * channel 被关闭后，还能继续从中 receive 数据，不过接收到的都是 chan type 中 type 的 zero value，比如 jobs 中即为 int 的 zero value 0，
+	 * 因此需要用第二个返回值，即 more 来判断 channel jobs 是否被关闭
 	 */
 	go func() {
 		for {
